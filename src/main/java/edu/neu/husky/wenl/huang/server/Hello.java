6b00c82bfdaab663a3079e8eed96aa5b4893d100
Wenlin Huang <jwong8593@gmail.com>
2017-09-26 22:14:00 -0700

    Simple HTTP Server

---
+++ src/main/java/edu/neu/husky/wenl/huang/server/Hello.java
@@ -0,0 +1,19 @@
+package edu.neu.husky.wenl.huang.server;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class Hello {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+        public String getStatus() {
+        return ("alive");
+    }
+
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+        public int postText(String content) {
+        return (content.length());
+    }
+}
