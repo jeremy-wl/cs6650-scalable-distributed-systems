ecc8aad32fa1af3b267b3ae1ca632441a4b02f14
Wenlin Huang <jwong8593@gmail.com>
2017-12-01 23:23:34 -0800

    Added lambda function for POST

--- src/main/aws-lambda/handler.js
+++ src/main/aws-lambda/handler.js
@@ -1,16 +1,29 @@
-'use strict';
+'use strict'
+const amqp = require('amqplib/callback_api')
+const username = 'admin'
+const password = 'admin'
+const queueHost = '34.216.61.206'
+const uri = ''.concat('amqp://', username, ':', password, '@', queueHost)
 
 module.exports.load = (event, context, callback) => {
+  amqp.connect(uri, function(err, conn) {
+    conn.createConfirmChannel(function(err, channel) {
+      const q = 'GET'
+
+      channel.assertQueue(q, {durable: true})
+      channel.sendToQueue(q, new Buffer('Hello World!'), {}, function () {
+        conn.close()
+      })
+    })
+  })
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
     }),
-  };
+  }
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+  callback(null, response)
+}
